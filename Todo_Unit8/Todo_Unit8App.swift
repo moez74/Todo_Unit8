e035f153a516b27a9d7c40e6c52ea8958619d1ed
Moez Rehman <moez.rehman.gcu@gmail.com>
2023-07-27 11:43:59 +0500

    Initial Commit

---
+++ Todo_Unit8/Todo_Unit8App.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_Unit8App.swift
+//  Todo_Unit8
+//
+//  Created by Moez Rehman on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Todo_Unit8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
